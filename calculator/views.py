de1464680bb575c91a58269f709d4da9b96accf1
Wajahat <wajahath533@gmail.com>
2024-02-21 13:00:12 +0500

    first commit

---
+++ calculator/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def calInterface(request):                    
+                    return render(request,"calinterface.html")
